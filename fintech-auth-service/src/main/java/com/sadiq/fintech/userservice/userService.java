57c1604b30d4ee2fe285b2ee5084cd4c8c5260a3
Arman Shaikh <skkarman789@gmail.com>
2026-09-18 10:20:14 +0530

    commit by Arman Shaikh

---
+++ fintech-auth-service/src/main/java/com/sadiq/fintech/userservice/userService.java
@@ -0,0 +1,7 @@
+package com.sadiq.fintech.userservice;
+
+public class userService {
+    public static void main(String[] args) {
+        System.out.println("hello, this is Arman working on this repository");
+    }
+}
