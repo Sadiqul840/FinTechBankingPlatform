95ceeacbd228af5e396b0222bf6dd9b95bbab5b6
Arman Shaikh <skkarman789@gmail.com>
2026-09-18 23:50:04 +0530

    adding microservice user service

---
+++ fintech-user-service/src/main/java/com/arman/fintech/user/FintechUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arman.fintech.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechUserServiceApplication.class, args);
+	}
+
+}
---
+++ fintech-user-service/src/test/java/com/arman/fintech/user/FintechUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arman.fintech.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
