9bce4d64004ea565915f47aff95d8dc51e7cf19f
Arman Shaikh <skkarman789@gmail.com>
2026-09-19 12:30:10 +0530

    some structural changes

--- fintech-user-service/src/main/java/com/arman/fintech/user/FintechUserServiceApplication.java
+++ fintech-user-service/src/main/java/com/arman/fintech/user/FintechUserServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FintechUserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FintechUserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FintechUserServiceApplication.class, args);
+    }
 
 }
---
+++ fintech-user-service/src/main/java/com/arman/fintech/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.arman.fintech.user.controller;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class UserController {
+    @GetMapping("/showuser")
+    public String showUser(){
+        return "hello my name is arman";
+
+    }
+
+}
